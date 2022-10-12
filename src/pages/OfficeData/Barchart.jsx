cbc3f538a64e2ea2f8751647e70a1e586030d088
jsink_ <andrikv@gmail.com>
2022-10-12 13:18:45 +0300

    Some layout fixes

--- src/pages/OfficeData/Barchart.jsx
+++ src/pages/OfficeData/Barchart.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import BarChartSkeleton from './BarChartSkeleton'
+import BarChartSkeleton from './BarchartSkeleton'
 import { Bar } from 'react-chartjs-2'
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js'
 
