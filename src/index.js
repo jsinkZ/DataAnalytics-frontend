298071546da9cd22bfc96384a6a1dfd137428dad
jsink_ <andrikv@gmail.com>
2022-11-25 12:52:59 +0300

    removed strict mode

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
 	<BrowserRouter>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
+		<App />
 	</BrowserRouter>
 )
