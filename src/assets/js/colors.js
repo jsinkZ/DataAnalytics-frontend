ddfdc1cc4f301b414ca36a95793f259913be3d43
jsink_ <andrikv@gmail.com>
2022-11-25 12:52:36 +0300

    file with colors created

---
+++ src/assets/js/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+	red: '#C17B7B',
+	backgroundRed: '#C18E8E',
+	pink: '#FF6384',
+	backgroundPink: '#FFB0C1',
+	blue: '#7BB2C1',
+	backgroundBlue: '#99D0F5',
+	purple: '#987BC1',
+	backgroundPurple: '#AEA1C1',
+	green: '#7BC1A4',
+	backgroundGreen: '#97C1AF',
+}
+
+export default colors
