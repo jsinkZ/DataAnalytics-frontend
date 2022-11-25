4156ac27df5356df79ad9a88b3401574dbacbe89
jsink_ <andrikv@gmail.com>
2022-11-25 12:54:18 +0300

    tag main outside routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import './App.scss'
-import Header from './components/Header'
+import Header from './components/Header/Header'
 import MillennialsData from './pages/millennialsData'
 import OfficeData from './pages/officeData'
 import PhysicsData from './pages/physicsData'
@@ -11,12 +11,15 @@ const App = () => {
 	return (
 		<div className='App'>
 			<Header />
-			<Routes>
-				<Route path='/' element={<OfficeData />} />
-				<Route path='/office' element={<OfficeData />} />
-				<Route path='/physics' element={<PhysicsData />} />
-				<Route path='/millennials' element={<MillennialsData />} />
-			</Routes>
+			<main>
+				<Routes>
+					<Route path='/' element={<OfficeData />} />
+					<Route path='/office' element={<OfficeData />} />
+					<Route path='/physics' element={<PhysicsData />} />
+					<Route path='/millennials' element={<MillennialsData />} />
+				</Routes>
+				<h2 className='rotatePhone'>Для лучшего использования переверните телефон </h2>
+			</main>
 		</div>
 	)
 }
